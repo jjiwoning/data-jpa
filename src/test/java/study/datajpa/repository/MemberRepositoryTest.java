17d94df3b073e386a1b09ff45e48e50906d7b7fc
JJiWoNing <jjiwoning12@gmail.com>
2022-08-31 15:03:20 +0900

    8/31 User definition repository

--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -290,4 +290,9 @@ class MemberRepositoryTest {
         List<Member> result = memberRepository.findLockByUsername("member1");
 
     }
+
+    @Test
+    void callCustom(){
+        List<Member> result = memberRepository.findMemberCustom();
+    }
 }
