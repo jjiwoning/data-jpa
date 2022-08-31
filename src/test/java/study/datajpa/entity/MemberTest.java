569df77a265abb01e97d67f9d29ccc10ba957a9b
JJiWoNing <jjiwoning12@gmail.com>
2022-08-31 15:32:49 +0900

    8/31 Auditing

--- src/test/java/study/datajpa/entity/MemberTest.java
+++ src/test/java/study/datajpa/entity/MemberTest.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
+import study.datajpa.repository.MemberRepository;
 
 import javax.persistence.EntityManager;
 
@@ -14,11 +15,14 @@ import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @Transactional
-@Rollback(value = false)
+//@Rollback(value = false)
 class MemberTest {
 
     @Autowired
     EntityManager em;
+
+    @Autowired
+    MemberRepository memberRepository;
 
     @Test
     public void testEntity(){
@@ -51,5 +55,29 @@ class MemberTest {
             System.out.println("member.getTeam() = " + member.getTeam());
             System.out.println("===========================");
         }
+    }
+
+    @Test
+    void jpaEventBaseEntity() throws Exception{
+        //given
+        Member member = new Member("member1");
+        memberRepository.save(member); //@PrePersist 동작
+
+        Thread.sleep(100);
+        member.setUsername("member2");
+
+        em.flush(); //@PreUpdate
+        em.clear();
+
+        //when
+        Member findMember = memberRepository.findById(member.getId()).get();
+
+        //then
+        System.out.println("findMember = " + findMember.getCreatedDate());
+//        System.out.println("findMember = " + findMember.getUpdatedDate());
+        System.out.println("findMember = " + findMember.getLastModifiedDate());
+        System.out.println("findMember = " + findMember.getCreatedBy());
+        System.out.println("findMember = " + findMember.getLastModifiedBy());
+
     }
 }
